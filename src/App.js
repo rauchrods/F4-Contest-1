b31be4d854c5a912dcde325b216e025e0d3ed52d
rauch <rauchrodrigues@gmail.com>
2023-05-13 13:25:24 +0530

    1st push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  let [result, setResult] = useState("");
+  let [num1, setnum1] = useState("");
+  let [num2, setnum2] = useState("");
+  let [msg, setmsg] = useState("");
+
+
+  function validation(e) {
+    if (num1 == "") {
+      setmsg("Error :  Num1 cannot be empty");
+      setResult("");
+      return;
+    }
+    else if (num2 == "") {
+      setmsg("Error :  Num2 cannot be empty");
+      setResult("");
+      return;
+    }
+    else {
+      setmsg("Success : Your result is shown above!");
+      calculate(e);
+    }
+  }
+
+  function calculate(e) {
+    if (e.target.innerText == "+") {
+      setResult(Number(num1) + Number(num2));
+    }
+    else if (e.target.innerText == "-") {
+      setResult(Number(num1) - Number(num2));
+    }
+    else if (e.target.innerText == "*") {
+      setResult(Number(num1) * Number(num2));
+    }
+    else {
+      setResult(Number(num1) / Number(num2));
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator-comp">
+      <h1>React Calculator</h1>
+      <input type="text" id="num1-inp" placeholder='Num 1' onChange={(e) => { setnum1(e.target.value) }} />
+      <input type="text" id="num2-inp" placeholder='Num 2' onChange={(e) => { setnum2(e.target.value) }} />
+      <div className="btn-grp">
+        <button onClick={validation}>+</button>
+        <button onClick={validation}>-</button>
+        <button onClick={validation}>*</button>
+        <button onClick={validation}>/</button>
+      </div>
+      <div className='res'>{result === "" ? "" : result}</div>
+      <div className={(msg=="Success : Your result is shown above!")?"success":"error"} >{msg}</div>
     </div>
   );
 }
